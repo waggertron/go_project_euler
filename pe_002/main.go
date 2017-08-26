148a23b3a98c1791cb2d0fdab5365c0e4420a025
waggertron <weylinwagnon@gmail.com>
2017-08-26 15:19:49 -0700

    add 2 and 3 solutions

--- pe_002/main.go
+++ pe_002/main.go
@@ -5,6 +5,14 @@ import (
 )
 
 func main() {
-
+	fmt.Println(sumEvenFib(4000000)
 }
 
+func sumEvenFib(end int) int {
+    x, y, sum := 1, 1, 0
+    for sum < end {
+        sum += x + y
+        x, y = x+2*y, 2*x+3*y
+    }
+    return sum
+}
--- pe_003/main.go
+++ pe_003/main.go
@@ -5,6 +5,17 @@ import (
 )
 
 func main() {
-
+	fmt.Println(findMaxFactor(600851475143))
 }
-
+func findMaxFactor(num int64) int64 {
+	factors := make([]int64, 0)
+	for i := int64(2); i <= num; i++ {
+		if num%i == 0 {
+			factors = append(factors, i)
+			for num%i == 0 {
+				num = num / i
+			}
+		}
+	}
+	return factors[len(factors)-1]
+}
