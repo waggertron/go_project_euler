0338f31ec43286e54c3ff37460d65f830eeb989a
waggertron <weylinwagnon@gmail.com>
2017-08-31 15:14:34 -0700

    add 9

--- pe_009/main.go
+++ pe_009/main.go
@@ -1,10 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
+	for c := 3; c <= 997; c++ {
+		for b := 2; b < c; b++ {
+			for a := 1; a < b; a++ {
+				if isValid(a, b, c) && a+b+c == 1000 {
+					fmt.Printf("a: %v, b: %v, c: %v\n", a, b, c)
+					fmt.Println(a * b * c)
+				}
+			}
+		}
+	}
 }
 
+func isValid(a, b, c int) bool {
+	return a < b && b < c && a*a+b*b == c*c
+}
